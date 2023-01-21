c8eca644d6b0acf246c24fa49e94bc69961f2b75
Clearsu <clearwater1219@gmail.com>
2023-01-21 19:23:30 +0900

    Fix : fixed heap buffer overflow

--- philo/set_start_time_and_unlock_all.c
+++ philo/set_start_time_and_unlock_all.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 04:03:00 by jincpark          #+#    #+#             */
-/*   Updated: 2022/12/19 20:23:29 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/01/21 19:22:12 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	set_start_and_limit(t_philo *philo, int n, time_t now, time_t limit)
 	i = 0;
 	if (n > 3)
 	{
-		while (i < n)
+		while (i < n - 3)
 		{
 			philo[i].time.start = now;
 			philo[i].limit = limit;
@@ -46,7 +46,7 @@ void	unlock_all_philos(pthread_mutex_t *mutex_philo, int n)
 	i = 0;
 	if (n > 3)
 	{
-		while (i < n)
+		while (i < n - 3)
 		{
 			pthread_mutex_unlock(&mutex_philo[i]);
 			pthread_mutex_unlock(&mutex_philo[i + 1]);
